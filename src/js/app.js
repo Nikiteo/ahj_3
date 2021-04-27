3966368bd2775d23405c1f16ef6b95b17fdc813d
Nikiteo <zhigiley.nikita2012@yandex.ru>
2021-04-27 09:07:29 +0300

    Добавлен счетчик и исправлена верстка

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
 const holes = document.querySelectorAll('.field_hole');
 const numOfHoles = holes.length;
 const randomField = () => Math.floor(Math.random() * numOfHoles);
+const successCount = document.querySelector('.success_count');
+const failedCount = document.querySelector('.failed_count');
 let position;
 let timer;
+let successIterator = 0;
 let loseIterator = 0;
 const goblin = document.createElement('img');
 goblin.classList.add('field_goblin');
@@ -39,11 +42,14 @@ holes.forEach((element) => {
     if (e.target.classList.contains('field_goblin')) {
       hideGoblin();
       reloadTimer();
+      successIterator += 1;
+      successCount.textContent = successIterator;
     } else if (loseIterator >= 4) {
       clearInterval(timer);
       hideGoblin();
     } else {
       loseIterator += 1;
+      failedCount.textContent = loseIterator;
     }
   });
 });
